d7218cbeb965ec7b4a1fa39f34e93ffc8d2179de
Arjun Maravan <napoleon@Arjun-Maravans-MacBook-Pro.local>
2020-04-28 21:27:53 +0530

    store code removed

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
-from resources.store import Store, StoreList
+#from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
@@ -18,8 +18,8 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(StoreList, '/stores')
+#api.add_resource(Store, '/store/<string:name>')
+#api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
